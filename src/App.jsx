39e10794538ce5ba20bdb886b43cb9ab1f4fddb3
gachaupg <gachaupg607@gmail.com>
2024-01-01 12:13:35 +0300

    udate

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import React, { Suspense, useCallback, useMemo } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Topbar from "./Componenents/Topbar";
+import Navbar from "./Componenents/Navbar";
+
+const loadHome = () => import("./pages/Home");
+// eslint-disable-next-line react/prop-types, no-unused-vars
+
+
+
+
+const Home = React.lazy(() => loadHome());
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <BrowserRouter>
+          {" "}
+          <Topbar /> 
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+        {/* <CallbackComponent /> */}
+        {/* <ExpensiveCalculationComponent/> */}
+      </Suspense>
+    </div>
+  );
 }
 
-export default App
+export default App;
