6f36ac60a58342e20b1e0671a9d2912eede52794
IsoDevMate <oumaoduor5827@gmail.com>
2024-01-10 12:04:50 +0300

    card with and links

--- src/card.jsx
+++ src/card.jsx
@@ -41,22 +41,22 @@ export default function GalleryVertical() {
           </CardContainer>
         ))
       ) : (
-        CardsData.map(({ imageurl, title, name, price }) => (
+        CardsData.map(({ imageurl, title, name, price,links }) => (
           <CardContainer
           key={title}
-          className="first:ms-auto  last:me-auto ring-1 ring-inset ring-neutral-200 shrink-0 rounded-md hover:shadow-lg  hover:text-black transition-colors w-[200px] lg:w-[192px] h-[350px]"
+          className="first:ms-auto  last:me-auto ring-1 ring-inset ring-neutral-200 shrink-0 rounded-md hover:shadow-lg  hover:text-black transition-colors w-[250px] lg:w-[192px] h-[400px]"
           animate={{ opacity: 1, transition: { duration: 0.7, delay: 0.2, type: 'spring', bounce: 0.25 }, y: 0 }}
           initial={{ opacity: 0, y: 100 }}
           whileHover={{ scale: 1.04 }}
         >
-          <div className="relative top-0 md:max-w-2xl lg:mx-auto">
-            <Link href="#" className="block">
+           <div className="relative top-0 md:max-w-2xl lg:mx-auto">
+           <a href={links}  target="_blank" rel="noopener noreferrer" className="block">
               <img
                 src={imageurl}
                 alt={title}
-                className="block object-cover h-1/4 rounded-md aspect-square w-[200px] lg:w-[190px] lg:h-[190px]"
+                className="block object-cover h-1/4 rounded-md aspect-square w-[250px] lg:w-[190px] lg:h-[190px]"
               />
-            </Link>
+            </a>
           </div>
           <div className="p-2 border-t border-neutral-200 typography-text-sm  flex flex-col  ">
             <Link href="#" className="block text-black  h-12 overflow-auto" style={{ textDecoration: 'none'}}>
